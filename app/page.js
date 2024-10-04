fd51ebb75a798af6cbfbd144ddbd17ba3e04e547
Sam Golshan <sam.golshan07@gmail.com>
2024-10-04 14:46:41 -0700

    Deleted console logs, minor changes to UI

--- app/page.js
+++ app/page.js
@@ -2,8 +2,8 @@ export default function Home() {
   return (
     <section>
       <div className="grid grid-cols-1 justify-items-center">
-        <h1 className='text-3xl text-center font-bold'>Welcome to supplyChain</h1>
-        <h2 className='text-base text-center'>A place where all your supply needs are met.</h2>
+        <h1 className='text-3xl text-center font-bold mt-4'>Welcome to supplyChain</h1>
+        <h2 className='text-base text-center mb-5'>Find out where your products have been</h2>
       </div>
       <div className="grid grid-cols-1 justify-items-center py-4">
         <div className='flex size-80  border-white rounded bg-gradient-to-r from-white mb-4'>
